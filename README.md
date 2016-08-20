961834a426fe4bb3562735054fa3e001623955bf
doried <doried.a.a@gmailc.com>
2016-08-20 23:54:46 +0300

    initial commit - files added to git

---
+++ README.md
@@ -0,0 +1,4 @@
+.checkout
+=========
+
+A Symfony project created on August 20, 2016, 11:17 pm.
---
+++ src/markety/TestBundle/marketyTestBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace markety\TestBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class marketyTestBundle extends Bundle
+{
+}
